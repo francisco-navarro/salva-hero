a3f518777abe265bb84da56e1561a5d26f381f57
Paco Navarro <packarbell@gmail.com>
2017-06-29 19:44:23 +0200

    Changes ports

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const express = require('express');
 const itemController = require('./item.controller');
 const app = express();
 
-app.listen(process.env.PORT || 4001);
+app.listen(process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 4001);
+
+app.get('/info', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.json({status: 'OK'});
+});
 
 app.get('/item', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
--- test/item.controller.test.js
+++ test/item.controller.test.js
@@ -2,3 +2,6 @@
 'localhost:4001/item?asin=B010EB3RZY&store=es'
 
 'localhost:4001/item?asin=B00LP3AMC2&store=es'
+
+'localhost:4001/item?asin=B01LSUYTJI&store=es'
+
