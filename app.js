f438acee48c39418e4c7a20065b91c98bf1f282f
Paco Navarro <packarbell@gmail.com>
2017-06-29 20:09:31 +0200

    Config for openshift

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require('express');
 const itemController = require('./item.controller');
 const app = express();
 
-app.listen(process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 4001);
+var server_port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 4001;
+var server_ip_address = process.env.HOST || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+app.listen(server_port, server_ip_address, function() {
+  console.log('App listening on port ' + server_port);
+});
+
 
 app.get('/info', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
