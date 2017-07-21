a7358f0a3f1983f8b5bd76b37f79964b25873b91
Paco Navarro <xe55471>
2017-07-21 09:46:28 +0200

    iffe and log

--- item.controller.js
+++ item.controller.js
@@ -1,108 +1,113 @@
-const cheerio = require('cheerio');
-const rp = require('request-promise');
-const request = require('request').defaults({
-  jar: true
-});
-const fakeCookie = require('./fake.cookies')();
-const itemParser = require('./item.parser');
-const proxiesController = require('./proxies.controller');
-const ProxyLists = require('proxy-lists');
-const http = require("http");
-const https = require("https");
+(function(){
+  const cheerio = require('cheerio');
+  const rp = require('request-promise');
+  const request = require('request').defaults({
+    jar: true
+  });
+  const fakeCookie = require('./fake.cookies')();
+  const itemParser = require('./item.parser');
+  const proxiesController = require('./proxies.controller');
+  const ProxyLists = require('proxy-lists');
+  const http = require("http");
+  const https = require("https");
 
-let lastPetition;
-let lastOk;
-let lastItem;
-let uptime = new Date();
+  let lastPetition;
+  let lastOk;
+  let lastItem;
+  let uptime = new Date();
 
-function get(asin, store) {
-  return new Promise((resolve, reject) =>  {
-    var proxy = proxiesController.nextProxy();
-    var body = '';
+  function get(asin, store) {
+    return new Promise((resolve, reject) =>  {
+      var proxy = proxiesController.nextProxy();
+      var body = '';
+      var chrome = 'Chrome/59.0.1' + Date.now() % 100000 / 1000;
+      var options = {
+        path: `https://www.amazon.${store}/gp/offer-listing/${asin}/ref=dp_olp_new_mbc?ie=UTF8&condition=new`,
+        headers: {
+          'Referer': 'https://www.amazon.es/s/ref=nb_sb_noss?__mk_es_ES=' + salt() + '&url=search-alias%3Daps&qid=' + Math.floor(Date.now() / 1000) + '&field-keywords=iphone' + asin,
+          'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) ' + chrome + ' Safari/537.36',
+          'Pragma': 'no-cache',
+          'host': 'www.amazon.es'
+        },
+        host: proxy.ipAddress,
+        port: proxy.port,
+        // jar: fakeCookie.get()
+      };
+      lastPetition = new Date();
+
+      http.get(options, function (res) {
+        res.setEncoding('utf8');
+        if (res.statusCode === 200) {
+
+        }
+        res.on('data', data => {
+          body += data;
+        });
+        res.on('end', () => {
+          try {
+            let item = itemParser.get(asin, cheerio.load(body));
+            if (item.price || item.primePrice){
+              lastOk = new Date();
+              resolve(item);
+            } else {
+              getLocal(asin, store).then(resp => {
+                lastOk = new Date();
+                resolve(resp)
+              });
+            }
+          } catch (err) {
+            console.warn('Error on proxy '+ proxy || err);
+            reject(err);
+          }
+        });
+      }).on('error', e => {
+        getLocal(asin, store)
+          .then(resp => {
+                resolve(resp);
+          }).catch((err) => reject("Got error: " + err));
+      })
+    });
+  }
+
+  function getLocal(asin, store) {
     var chrome = 'Chrome/59.0.1' + Date.now() % 100000 / 1000;
     var options = {
-      path: `https://www.amazon.${store}/gp/offer-listing/${asin}/ref=dp_olp_new_mbc?ie=UTF8&condition=new`,
-      headers: {
-        'Referer': 'https://www.amazon.es/s/ref=nb_sb_noss?__mk_es_ES=' + salt() + '&url=search-alias%3Daps&qid=' + Math.floor(Date.now() / 1000) + '&field-keywords=iphone' + asin,
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) ' + chrome + ' Safari/537.36',
-        'Pragma': 'no-cache',
-        'host': 'www.amazon.es'
-      },
-      host: proxy.ipAddress,
-      port: proxy.port,
-      // jar: fakeCookie.get()
-    };
-    lastPetition = new Date();
-
-    http.get(options, function (res) {
-      res.setEncoding('utf8');
-      if (res.statusCode === 200) {
-
-      }
-      res.on('data', data => {
-        body += data;
-      });
-      res.on('end', () => {
-        try {
-          let item = itemParser.get(asin, cheerio.load(body));
-          if (item.price || item.primePrice){
-            lastOk = new Date();
-            resolve(item);
-          } else {
-            getLocal(asin, store).then(resp => {
-              lastOk = new Date();
-              resolve(resp)
-            });
-          }
-        } catch (err) {
-          console.warn('Error on proxy '+ proxy || err);
-          reject(err);
+        uri: `http://www.amazon.${store}/gp/offer-listing/${asin}/ref=dp_olp_new_mbc?ie=UTF8&condition=new`,
+        headers: {
+          'Referer': 'https://www.amazon.es/s/ref=nb_sb_noss?__mk_es_ES=' + salt() + '&url=search-alias%3Daps&qid=' + Math.floor(Date.now() / 1000) + '&field-keywords=iphone' + asin,
+          'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) ' + chrome + ' Safari/537.36',
+          'Pragma': 'no-cache',
+          'host': 'www.amazon.es'
+        },
+        jar: fakeCookie.get(),
+        transform: function (body) {
+          return cheerio.load(body);
         }
+      };
+    console.log('Getting from local...');
+    return rp(options)
+      .then(function ($) {
+        let item =  itemParser.get(asin, $);
+        if(item.price || item.primePrice ) console.log('... OK');
+        return item;
       });
-    }).on('error', e => {
-      getLocal(asin, store)
-        .then(resp => {
-              resolve(resp);
-        }).catch((err) => reject("Got error: " + err));
-    })
-  });
-}
+  }
 
-function getLocal(asin, store) {
-  var chrome = 'Chrome/59.0.1' + Date.now() % 100000 / 1000;
-  var options = {
-      uri: `http://www.amazon.${store}/gp/offer-listing/${asin}/ref=dp_olp_new_mbc?ie=UTF8&condition=new`,
-      headers: {
-        'Referer': 'https://www.amazon.es/s/ref=nb_sb_noss?__mk_es_ES=' + salt() + '&url=search-alias%3Daps&qid=' + Math.floor(Date.now() / 1000) + '&field-keywords=iphone' + asin,
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) ' + chrome + ' Safari/537.36',
-        'Pragma': 'no-cache',
-        'host': 'www.amazon.es'
-      },
-      jar: fakeCookie.get(),
-      transform: function (body) {
-        return cheerio.load(body);
-      }
+  function salt() {
+    var len = 10 + parseInt(Math.random() * 20) + Date.now() % 20;
+    return encodeURI('iphoneÁMöOÒZNáéíóúìèàiphoneÁMöOÒZNáéíóúìèà'.split ``.sort(a => .5 - Math.random()).join ``.substr(0, len))
+  }
+
+  function status() {
+    return {
+      uptime,
+      lastPetition,
+      lastOk,
+      lastItem
     };
-  return rp(options)
-    .then(function ($) {
-      return itemParser.get(asin, $);
-    });
-}
-
-function salt() {
-  var len = 10 + parseInt(Math.random() * 20) + Date.now() % 20;
-  return encodeURI('iphoneÁMöOÒZNáéíóúìèàiphoneÁMöOÒZNáéíóúìèà'.split ``.sort(a => .5 - Math.random()).join ``.substr(0, len))
-}
-
-function status() {
-  return {
-    uptime,
-    lastPetition,
-    lastOk,
-    lastItem
+  }
+  module.exports = {
+    get: get,
+    status: status
   };
-}
-module.exports = {
-  get: get,
-  status: status
-};
+})();
--- proxies.controller.js
+++ proxies.controller.js
@@ -1,75 +1,77 @@
-const ProxyLists = require('proxy-lists');
-const http = require("http");
+(function(){
+  const ProxyLists = require('proxy-lists');
+  const http = require("http");
 
-const MAX_TIMEOUT_PROXY = 12000;
+  const MAX_TIMEOUT_PROXY = 12000;
 
-const options = {
-  protocols: ['https', 'http'],
-  bitproxies: {
-    apiKey: process.env.BITPROXY
-  }
-};
-
-let proxyList = [];
-let count = 0;
-
-function pool() {
-  let gettingProxies = ProxyLists.getProxies(options);
-  proxyList = [];
-
-  gettingProxies.on('data', function (proxies) {
-    if (proxies.length) {
-      proxies.map(el => {
-        var options = {
-          host: el.ipAddress,
-          port: el.port,
-          path: "https://www.amazon.es/s/ref=nb_sb_noss_2?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&url=search-alias%3Daps&field-keywords=kindle",
-          headers: {
-            Host: "www.amazon.es"
-          }
-        };
-        var date1 = Date.now();
-        http.get(options, function (res) {
-          if (res.statusCode === 200) {
-            var body = '';
-            res.on('data', data => {
-              body += data;
-            });
-            res.on('end', () => {
-              if (body.match('body') && body.match(/EUR [0-9]/) && !body.match(/Captcha/)) {
-                //Compruebo que este mas próximo de 15000ms
-                if ((Date.now() - date1) < MAX_TIMEOUT_PROXY) {
-                  proxyList.push(el);
-                  console.log('>>>>>>>> proxies total ' + proxyList.length)
-                }
-              }
-            });
-          }
-        }).on('error', function (e) {
-          //console.log("Got error: " + e.message);
-        });
-      });
+  const options = {
+    protocols: ['https', 'http'],
+    bitproxies: {
+      apiKey: process.env.BITPROXY
     }
-  });
+  };
 
-  gettingProxies.on('error', function (error) {
-    console.warn(error);
-  });
+  let proxyList = [];
+  let count = 0;
 
-  gettingProxies.once('end', function () {
-    console.log('>>>>>>>> proxies total ' + proxyList.length);
-  });
-}
+  function pool() {
+    let gettingProxies = ProxyLists.getProxies(options);
+    proxyList = [];
 
-pool();
+    gettingProxies.on('data', function (proxies) {
+      if (proxies.length) {
+        proxies.map(el => {
+          var options = {
+            host: el.ipAddress,
+            port: el.port,
+            path: "https://www.amazon.es/s/ref=nb_sb_noss_2?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&url=search-alias%3Daps&field-keywords=kindle",
+            headers: {
+              Host: "www.amazon.es"
+            }
+          };
+          var date1 = Date.now();
+          http.get(options, function (res) {
+            if (res.statusCode === 200) {
+              var body = '';
+              res.on('data', data => {
+                body += data;
+              });
+              res.on('end', () => {
+                if (body.match('body') && body.match(/EUR [0-9]/) && !body.match(/Captcha/)) {
+                  //Compruebo que este mas próximo de 15000ms
+                  if ((Date.now() - date1) < MAX_TIMEOUT_PROXY) {
+                    proxyList.push(el);
+                    console.log('>>>>>>>> proxies total ' + proxyList.length)
+                  }
+                }
+              });
+            }
+          }).on('error', function (e) {
+            //console.log("Got error: " + e.message);
+          });
+        });
+      }
+    });
 
-setInterval(pool, 60*60*1000);
+    gettingProxies.on('error', function (error) {
+      console.warn(error);
+    });
 
-function nextProxy(){
-    count = ((count || 0) + 1) % proxyList.length;
-    return proxyList[count] || {};
-}
+    gettingProxies.once('end', function () {
+      console.log('>>>>>>>> proxies total ' + proxyList.length);
+    });
+  }
 
-module.exports = {
-  nextProxy
-}
+  pool();
+
+  setInterval(pool, 60*60*1000);
+
+  function nextProxy(){
+      count = ((count || 0) + 1) % proxyList.length;
+      return proxyList[count] || {};
+  }
+
+  module.exports = {
+    nextProxy
+  }
+})();
